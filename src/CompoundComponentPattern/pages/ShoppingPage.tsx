9bea544fbbedd37e64b7dc271c67082dfe8c9f65
@FranCarn <carnevalefranco99@gmail.com>
2023-08-30 13:24:05 -0300

    Control Props from father

--- src/CompoundComponentPattern/pages/ShoppingPage.tsx
+++ src/CompoundComponentPattern/pages/ShoppingPage.tsx
@@ -32,19 +32,36 @@ export const ShoppingPage = () => {
 
   const onProductCountChange = ({ count, product }: OnChangeArgs) => {
     setShoppingCart((prev) => {
-      if (!count) {
-        const { [product.id]: toDelete, ...rest } = prev;
-        return rest;
-      }
-      return {
-        ...prev,
-        [product.id]: {
-          ...product,
-          count,
-        },
+      // resolve like that if child control the state
+      // if (!count) {
+      //   const { [product.id]: toDelete, ...rest } = prev;
+      //   return rest;
+      // }
+      // return {
+      //   ...prev,
+      //   [product.id]: {
+      //     ...product,
+      //     count,
+      //   },
+
+      const productInCart: ProductInCart = prev[product.id] || {
+        ...product,
+        count: 0,
       };
+
+      if (Math.max(productInCart.count + count, 0) > 0) {
+        productInCart.count += count;
+        return {
+          ...prev,
+          [product.id]: productInCart,
+        };
+      }
+
+      const { [product.id]: toDelete, ...rest } = prev;
+      return rest;
     });
   };
+
   return (
     <div>
       <h1>Store</h1>
