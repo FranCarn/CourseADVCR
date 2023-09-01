28f169bd2a8155c8f131d891664b4f68a14faae4
@FranCarn <carnevalefranco99@gmail.com>
2023-09-01 12:56:31 -0300

    Get info from form ready

--- src/03-Forms/pages/FormikBasicPage.tsx
+++ src/03-Forms/pages/FormikBasicPage.tsx
@@ -1,21 +1,48 @@
-import React from "react";
+import { useFormik } from "formik";
+
 import "../styles/styles.css";
 export const FormikBasicPage = () => {
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik Basic</h1>
 
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={values.firstName}
+        />
         <span>First name is required</span>
 
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={values.lastName}
+        />
         <span>Last name is required</span>
 
         <label htmlFor="email">Email address</label>
-        <input type="email" name="email" />
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={values.email}
+        />
         <span>Email is required</span>
         <span>Invalid Email</span>
 
