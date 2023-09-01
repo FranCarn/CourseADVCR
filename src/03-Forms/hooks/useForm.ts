9e5a0524990333f4fb6215f120be2b86c6a0367f
@FranCarn <carnevalefranco99@gmail.com>
2023-09-01 12:36:01 -0300

    expand funcionalities of useForm

--- src/03-Forms/hooks/useForm.ts
+++ src/03-Forms/hooks/useForm.ts
@@ -11,9 +11,21 @@ export const useForm = <T>(initialValue: T) => {
     }));
   };
 
+  const resetForm = () => {
+    setFormData({ ...initialValue });
+  };
+
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
   return {
     ...formData,
     formData,
     handleChange,
+    isValidEmail,
+    resetForm,
   };
 };
